f618c4327a571234f89568a157a2b446057b65e6
Patrick Fieldson <fieldson.patrick@gmail.com>
2023-12-03 16:59:57 -0800

    Rename interoperability directory, fix misc bugs

--- app/modules/DisplayManager/lib/Import.ps1
+++ app/modules/DisplayManager/lib/Import.ps1
@@ -1,6 +1,6 @@
 # Simple script to import the public portion of all libraries in this directory
 Set-Location $PSScriptRoot
-Add-Type -Path .\DisplayInterop\DisplayConfig.cs -IgnoreWarnings
-Add-Type -Path .\DisplayInterop\DisplayDevices.cs -IgnoreWarnings
-Add-Type -Path .\DisplayInterop\DisplaySettings.cs -IgnoreWarnings
-Add-Type -Path .\DisplayInterop\Win32Errors.cs -IgnoreWarnings
+Add-Type -Path .\Win32Interop\DisplayConfig.cs -IgnoreWarnings
+Add-Type -Path .\Win32Interop\DisplayDevices.cs -IgnoreWarnings
+Add-Type -Path .\Win32Interop\DisplaySettings.cs -IgnoreWarnings
+Add-Type -Path .\Win32Interop\Win32Errors.cs -IgnoreWarnings
